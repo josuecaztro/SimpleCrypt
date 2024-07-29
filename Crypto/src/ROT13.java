4e96dfa8f0b378eeb1ec0c29add1440c2b8c5673
Josue <josue@zipcodersmbp75.lan>
2024-07-28 20:51:00 -0400

    Finished

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -12,21 +12,33 @@ public class ROT13  {
 
 
     public String crypt(String text) throws UnsupportedOperationException {
-
-        return "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (c >= 'a' && c <= 'm')
+                c += 13;
+            else if (c >= 'A' && c <= 'M')
+                c += 13;
+            else if (c >= 'n' && c <= 'z')
+                c -= 13;
+            else if (c >= 'N' && c <= 'Z')
+                c -= 13;
+            sb.append(c);
+        }
+        return sb.toString();
     }
 
     public String encrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public String decrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public static String rotate(String s, Character c) {
-
-        return "";
+        int lastIndex = s.indexOf(c);
+        return s.substring(lastIndex) + s.substring(0, lastIndex);
     }
 
 }
